e8a09b8ab0b2367a70921315be35e63043afb414
nailahadayan <nailahadyan19@gmail.com>
2024-02-27 08:05:47 +0700

    tugas bidata

---
+++ app/src/main/java/com/example/nailahkotlin/hello.kt
@@ -0,0 +1,31 @@
+package com.example.nailahkotlin
+
+class Biodata(
+    val nama: String,
+    val TTL: String,
+    val umur: Int,
+    val alamat: String,
+    val hobi: List<String>
+) {
+    fun displayBiodata() {
+        println("===== BIODATA PRIBADI =====")
+        println("Nama: $nama")
+        println("TTL: $TTL")
+        println("Umur: $umur tahun")
+        println("Alamat: $alamat")
+        println("Hobi: ")
+        hobi.forEach { println("- $it") }
+    }
+}
+
+fun main() {
+    val biodata = Biodata(
+        nama = "Nailah Adyan Nurahmah",
+        TTL = "Ponorogo, 19 Februari 2005",
+        umur = 19,
+        alamat = "Perumahan Bumi Winongo Indah 2 Blok B",
+        hobi = listOf("Tidur Seharian", "Scroll Shopee")
+    )
+
+    biodata.displayBiodata()
+}
