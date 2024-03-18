442f541dbef9408148f517f706316fcff9bd6f86
nailahadayan <nailahadyan19@gmail.com>
2024-03-18 22:20:24 +0700

    tugas lanjutan

---
+++ app/src/main/java/com/example/nailahkotlin/house.kt
@@ -0,0 +1,27 @@
+package com.example.nailahkotlin
+
+class house {
+    val houseColor:String = "white"
+    val numberofWindows = 2
+    val isForSale = false
+
+    fun updateColor(){
+
+    }
+
+    fun put0nsale() {
+
+    }
+}
+
+fun main(){
+    val A1 = house()
+    val A2 = house()
+    val A3 = house()
+
+    println(A1.houseColor)
+    println("=".repeat(20))
+    println(A2)
+    println("=".repeat(20))
+    println(A3)
+}
---
+++ app/src/main/java/example/myapp/ decor/Main.kt
@@ -0,0 +1,13 @@
+package example.myapp.decor
+import java.lang.Math.PI
+
+fun buildAquarium() {
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+}
+fun main() {
+    buildAquarium()
+
+}
